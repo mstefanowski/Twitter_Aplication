f8b0cfe34addda57a2b3d72fef9e661cadb3cb97
Maciej Stefanowski <maciej.stefanowski6@gmail.com>
2019-06-06 23:01:06 +0200

    Otwieranie ekranu logowania

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from Twitter import Twitter_client
+from PyQt4 import QtGui
+import sys
+from okna.login_widget import Ui_Logowanie
 
 twitter = Twitter_client()
 
@@ -6,3 +9,12 @@ twitter.login()
 print(twitter.get_user_data())
 print(twitter.get_latest_tweets())
 
+def run():
+    app = QtGui.QApplication(sys.argv)
+    window = QtGui.QMainWindow()
+    Login_Window = Ui_Logowanie()
+    Login_Window.setupUi(window)
+    window.show()
+    sys.exit(app.exec())
+
+run()
