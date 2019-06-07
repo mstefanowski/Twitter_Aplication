290283bb1e072ce139a2f5c5e0a237650a7240a3
Maciej Stefanowski <maciej.stefanowski6@gmail.com>
2019-06-07 12:44:55 +0200

    zamknięcie okna logowania i otworzenie głównego okna po kliknięciu przycisku zalogowania

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from Twitter import Twitter_client
 from PyQt4 import QtGui
 import sys
 from okna.login_widget import Ui_Logowanie
+from okna.main_window import Ui_main_window
 
 twitter = Twitter_client()
 
 
 def run():
     app = QtGui.QApplication(sys.argv)
-    window = QtGui.QMainWindow()
+
+    login_dialog = QtGui.QDialog()
     login_Window = Ui_Logowanie(twitter)
-    login_Window.setupUi(window)
-    window.show()
-    sys.exit(app.exec())
+    login_Window.setupUi(login_dialog)
+    login_dialog.exec()
+
+    if(login_Window.loged):
+        window = QtGui.QMainWindow()
+        main_window = Ui_main_window()
+        main_window.setupUi(window)
+        window.show()
+        sys.exit(app.exec())
 
 run()
--- okna/login_widget.py
+++ okna/login_widget.py
@@ -26,48 +26,52 @@ class Ui_Logowanie(object):
 
     def __init__(self, twitter):
         self.twitter = twitter
+        self.loged = False
 
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("Logowanie"))
-        MainWindow.resize(400, 300)
+    def setupUi(self, parent):
+        self.current_window = parent
+        parent.setObjectName(_fromUtf8("Logowanie"))
+        parent.resize(400, 300)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
-        MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QtCore.QSize(400, 300))
-        MainWindow.setMaximumSize(QtCore.QSize(400, 300))
-        self.login_field = QtGui.QLineEdit(MainWindow)
+        sizePolicy.setHeightForWidth(parent.sizePolicy().hasHeightForWidth())
+        parent.setSizePolicy(sizePolicy)
+        parent.setMinimumSize(QtCore.QSize(400, 300))
+        parent.setMaximumSize(QtCore.QSize(400, 300))
+        self.login_field = QtGui.QLineEdit(parent)
         self.login_field.setGeometry(QtCore.QRect(150, 70, 221, 41))
         self.login_field.setObjectName(_fromUtf8("login_browser"))
-        self.password_field = QtGui.QLineEdit(MainWindow)
+        self.password_field = QtGui.QLineEdit(parent)
         self.password_field.setGeometry(QtCore.QRect(150, 130, 221, 41))
         self.password_field.setObjectName(_fromUtf8("password_browser"))
-        self.main_label = QtGui.QLabel(MainWindow)
+        self.main_label = QtGui.QLabel(parent)
         self.main_label.setGeometry(QtCore.QRect(160, 20, 71, 17))
         self.main_label.setObjectName(_fromUtf8("main_label"))
-        self.btn_logIn = QtGui.QPushButton(MainWindow)
+        self.btn_logIn = QtGui.QPushButton(parent)
         self.btn_logIn.setGeometry(QtCore.QRect(246, 216, 121, 51))
         self.btn_logIn.setObjectName(_fromUtf8("btn_logIn"))
         self.btn_logIn.clicked.connect(self.login_clicked)
-        self.login_label = QtGui.QLabel(MainWindow)
+        self.login_label = QtGui.QLabel(parent)
         self.login_label.setGeometry(QtCore.QRect(50, 80, 66, 17))
         self.login_label.setObjectName(_fromUtf8("login_label"))
-        self.password_label = QtGui.QLabel(MainWindow)
+        self.password_label = QtGui.QLabel(parent)
         self.password_label.setGeometry(QtCore.QRect(50, 140, 66, 17))
         self.password_label.setObjectName(_fromUtf8("password_label"))
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(parent)
+        QtCore.QMetaObject.connectSlotsByName(parent)
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("Logowanie", "Zaloguj się", None))
+    def retranslateUi(self, parent):
+        parent.setWindowTitle(_translate("Logowanie", "Zaloguj się", None))
         self.main_label.setText(_translate("Logowanie", "Zaloguj się", None))
         self.btn_logIn.setText(_translate("Logowanie", "Zaloguj się", None))
         self.login_label.setText(_translate("Logowanie", "Login", None))
         self.password_label.setText(_translate("Logowanie", "Hasło", None))
 
     def login_clicked(self):
+        self.loged = True
         self.twitter.login()
         print(self.twitter.get_user_data())
         print(self.twitter.get_latest_tweets())
+        self.current_window.hide()
